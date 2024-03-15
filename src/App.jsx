671c9fb4416a316150196f82574b3dd7e9c67e42
Munir <smsirajulmonir@gmail.com>
2024-03-15 13:52:14 +0600

    Navbar open close button created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 
 import './App.css'
+import DaisyNav from './Components/DaisyNav/DaisyNav'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar></Navbar>
+      <DaisyNav></DaisyNav>
       <h1 className='text-3xl bg-rose-600'>Vite + React</h1>
     </>
   )
